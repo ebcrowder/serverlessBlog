0b0d78567bcdaccae5cc2ea6de24ce9f4976054a
ebcrowder <ebcrowder@gmail.com>
2018-03-08 20:05:16 -0800

    server and gitignore update

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json({ strict: false }));
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header(
     'Access-Control-Allow-Methods',
     'POST, GET, OPTIONS, PUT, PATCH, DELETE'
